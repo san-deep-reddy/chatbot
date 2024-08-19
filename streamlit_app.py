1d95e19f2ea2275eecb167ad8566ceaf3d37e87b
Sandeep Reddy <60704433+san-deep-reddy@users.noreply.github.com>
2024-08-19 11:36:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ def setup_sidebar():
     # Privacy
     st.sidebar.markdown(
     """
-    <div style="font-size: 1px; margin-top: 70px;">
+    <div style="font-size: 1px; margin-top: 60px;">
         <p>Privacy: We do not retain and store user data from the chat session.</p>
     </div>
     """,
